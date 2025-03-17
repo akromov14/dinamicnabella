d9f49aa5afef62ac7807f7ee7055441885abb1bb
akromov14 <85354852+akromov14@users.noreply.github.com>
2025-03-17 21:28:14 +0500

    Add files via upload

--- nabellasite/loader.js
+++
@@ -1,5 +0,0 @@
-// Spinnerni 1 soniyadan keyin yo'qotish
-setTimeout(() => {
-    document.getElementById("loader").style.display = "none";
-    document.getElementById("content").style.display = "block";
-}, 1000);
